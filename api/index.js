1f5b31049b0b8a3ed484d33f698565cbce119797
arjun puri <thevirus991@gmail.com>
2024-07-12 01:56:55 +0530

    as

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use(express.static('./public/uploads'));
 // Use your API router
 app.use(apiRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
